a0b1ce433d5cb11e22794d91fd01b2e004a1b4e6
Paul Wentzel <paul.wentzel@gmail.com>
2024-08-01 21:56:28 -0400

    practice complete

--- data.js
+++ data.js
@@ -1,1 +1,20 @@
 // Your code here
+document.addEventListener('DOMContentLoaded', event => {
+  const addButton = document.getElementById('add');
+
+  const addToList = event => {
+    const shoppingList = document.getElementById('shopping-list');
+    const nameInput = document.getElementById('name');
+    const typeInput = document.getElementById('type');
+
+    let newLi = document.createElement('li');
+    newLi.innerText = nameInput.value.trim();
+    newLi.setAttribute("data-type", typeInput.value);
+
+    shoppingList.appendChild(newLi);
+    event.preventDefault();
+  }
+
+  addButton.addEventListener('click', addToList);
+
+});
